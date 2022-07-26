da4906968a1668d98ed947b891af7cbfacfcf843
steve austin <austines811@gmail.com>
2022-07-27 01:04:20 +0300

    adds footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer (){
+    return(
+        <>
+            <div class="footer">
+                <h5>©2022 Talii Pamoja™</h5> 
+            </div>
+        </>
+    )
+}
+
+export default Footer;
