7482cac51ae5412a50be8db3d0480255ab6b57e6
steve austin <austines811@gmail.com>
2022-07-27 14:51:21 +0300

    adds image and styling

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -18,16 +18,17 @@ function Scene({ scene, onDeleteScene }){
             onDeleteScene(id)
         })
     }
+ 
 
     return (
         <>
             <div className="oneSpecies">
-                {/* <img className="imgs" src={ require('${image}')} alt="img" /> */}
-                <h5 className="speciesName">${name}</h5>
-                <p className="specieDescription">${desc}</p>
-                <div id="seenBtnCarrier" className="withBtn"><button class="buttons" id="seenBtn" onClick={shiftIsSeen}>{ isSeen ? "Seen On Trail!🥳" : "Not seen"  }</button></div> 
-                <div  className="withBtn"><button class="buttons" onClick={deleteScene}>Delete Attraction</button></div> 
-                <div id="donateBtnCarrier" className="withBtn"><button class="buttons" id="donateBtn"> Donate $10 </button></div>
+                <img className="imgs" src = {image} alt='something'/>
+                <h5 className="speciesName">{name}</h5>
+                <p className="specieDescription">{desc}</p>
+                <div id="seenBtnCarrier" className="withBtn"><button className="buttons" id="seenBtn" onClick={shiftIsSeen}>{ isSeen ? "Seen On Trail!🥳" : "Not seen"  }</button></div> 
+                <div  className="withBtn"><button className="buttons" onClick={deleteScene}>Delete Attraction</button></div> 
+                <div id="donateBtnCarrier" className="withBtn"><button className="buttons" id="donateBtn"> Donate $10 </button></div>
             </div>
         </>
     )
