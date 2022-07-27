310c308bfe071a5a1befa0398e696ecb89e52a1a
steve austin <austines811@gmail.com>
2022-07-27 16:59:27 +0300

    polishes scenery apperance on DOM

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -26,9 +26,12 @@ function Scene({ scene, onDeleteScene }){
                 <img className="imgs" src = {image} alt='something'/>
                 <h5 className="speciesName">{name}</h5>
                 <p className="specieDescription">{desc}</p>
-                <div id="seenBtnCarrier" className="withBtn"><button className="buttons" id="seenBtn" onClick={shiftIsSeen}>{ isSeen ? "Seen On Trail!🥳" : "Not seen"  }</button></div> 
+                <div className="withBtn">
+                   <div className="withBtn"> <button id="seenBtnCarrier" className="buttons" onClick={shiftIsSeen}>{ isSeen ? "Seen On Trail!🥳" : "Not seen"  }</button></div>
+                    <div id="donateBtnCarrier" className="withBtn"><button className="buttons" id="donateBtn"> Donate $10 </button></div>
+                </div> 
                 <div  className="withBtn"><button className="buttons" onClick={deleteScene}>Delete Attraction</button></div> 
-                <div id="donateBtnCarrier" className="withBtn"><button className="buttons" id="donateBtn"> Donate $10 </button></div>
+                
             </div>
         </>
     )
